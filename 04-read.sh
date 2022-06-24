910daa8383fc3a4ebf4a4b7bbafbe3fdee3c9bc9
sripadv <sripadv123@gmail.com>
2022-06-25 00:33:11 +0530

    Demo

--- 04-read.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p 'Enter your name: ' name
-echo -e "Hey $name, How can i help you"
-
-read -p 'Enter your phone number: ' phone
-echo -e "Thanks, will text you details soon to $phone"
--- 05-special-vars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo script name = $0
-echo first input = $1
-echo second input = $2
-echo all inputs = $*
-echo Number of inputs = $#
