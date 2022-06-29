5b2361ca7b2ca1111a6668274e9a0c935676e7d9
sripadv <sripadv123@gmail.com>
2022-06-29 23:30:16 +0530

    updating ./RoboShop-devops65

--- components/mongodb.sh
+++ components/mongodb.sh
@@ -4,7 +4,7 @@ yum install -y mongodb-org
 systemctl enable mongod
 systemctl start mongod
 
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl restart mongod
 
